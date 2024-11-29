f8d690c8e0e4ed44068b79fece3900b7ef88e066
Shadi Samadi <s-samadi@github.com>
2024-11-29 11:40:31 +1100

    Update main.go

--- gallery/main.go
+++ gallery/main.go
@@ -196,7 +196,7 @@ func (g *Gallery) Create(profile *OctoProfile) error {
 func (g Gallery) Update(profile *OctoProfile) error {
 	db := GetDb()
 	query := fmt.Sprintf("UPDATE gallery SET title = '%s', description = '%s' WHERE id = %d and login = '%s'", g.Title, g.Description, g.ID, profile.Login)
-
+	//codeql[go/sql-injection]
 	stmt, err := db.Prepare(query)
 	if err != nil {
 		return err
